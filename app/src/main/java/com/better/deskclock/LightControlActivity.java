a84f44822a56d21f773cac64b055462b549461c0
Camilo Alejandro Arboleda <camilo@ieee.org>
2015-11-11 14:22:30 +0100

    Add buttons for light control
    They don't do anything yet.

--- app/src/main/java/com/better/deskclock/LightControlActivity.java
+++ app/src/main/java/com/better/deskclock/LightControlActivity.java
@@ -1,3 +1,7 @@
+/**
+ * Created by caar on 2015-11-09.
+ */
+
 package com.better.deskclock;
 
 import android.app.Activity;
@@ -6,12 +10,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-/**
- * Created by caar on 2015-11-09.
- */
 public class LightControlActivity extends Activity {
+    private class LightRegisters {
+        public int stepDirection = 0;
+        public int stepCounter = -1;
+        public int output = -1;
+        public int waitTime = -1;
+    }
+    // Register related constants
+    final int MAX_REGISTER_ADDRESS = 32;
+    final int STEP_REGISTER = 0;
+    final int LIGHT_REGISTER = 1;
+    final int STEP_COUNTER_REGISTER = 2;
+    final int WAIT_COUNTER_REGISTER = 4;
+    final int WARM_LIGHT_ADDRESS = 0;
+    final int COLD_LIGHT_ADDRESS = 16;
+
+    private boolean lightOn = false;
+
     private static final String TAG = "LightFragment";
     /**
      * Local Bluetooth adapter
@@ -31,12 +50,35 @@ public class LightControlActivity extends Activity {
 
         // If the adapter is null, then Bluetooth is not supported
         if (mBluetoothAdapter == null) {
-            //FragmentActivity activity = Activity.this.getActivity
-            //Toast.makeText(activity, "Bluetooth is not available", Toast.LENGTH_LONG).show();
-            //activity.finish();
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            this.finish();
         }
 
         updateLayout();
+    }
+
+    public void cycleLight(View v) {
+        if (!lightOn) {
+            LightRegisters onRegisters = new LightRegisters();
+            onRegisters.stepDirection = 1;
+            onRegisters.output = 0;
+            onRegisters.stepCounter = 127;
+            onRegisters.waitTime = 1;
+            updateLight(WARM_LIGHT_ADDRESS, onRegisters);
+            updateLight(COLD_LIGHT_ADDRESS, onRegisters);
+            Toast.makeText(this, "Light is on", Toast.LENGTH_SHORT).show();
+            lightOn = true;
+        } else {
+            LightRegisters offRegisters = new LightRegisters();
+            offRegisters.stepDirection = 255;
+            offRegisters.output = 127;
+            offRegisters.stepCounter = 127;
+            offRegisters.waitTime = 1;
+            updateLight(WARM_LIGHT_ADDRESS, offRegisters);
+            updateLight(COLD_LIGHT_ADDRESS, offRegisters);
+            Toast.makeText(this, "Light is off", Toast.LENGTH_SHORT).show();
+            lightOn = false;
+        }
     }
 
     private void updateLayout() {
@@ -49,6 +91,32 @@ public class LightControlActivity extends Activity {
             }
         });
 
-
     }
+
+    /* Light registers must be updated in the given order.
+    * */
+    private void updateLight(int lightNumber, LightRegisters registers) {
+        if (registers.waitTime >= 0) {
+            int w_low = registers.waitTime % 256;
+            int w_hi = registers.waitTime / 256;
+            writeRegister(lightNumber + WAIT_COUNTER_REGISTER, w_low);
+            writeRegister(lightNumber + WAIT_COUNTER_REGISTER + 1, w_hi);
+        }
+        writeRegister(lightNumber + STEP_COUNTER_REGISTER, registers.stepCounter);
+        writeRegister(lightNumber + LIGHT_REGISTER, registers.output);
+        writeRegister(lightNumber + STEP_REGISTER, registers.stepDirection);
+    }
+
+    private int writeRegister(int regNumber, int regValue) {
+        String btMessage;
+        if (regValue == -1)
+            return -1;
+        if (regNumber > MAX_REGISTER_ADDRESS || regNumber < 0)
+            return -1;
+        byte val = (byte) regValue;
+        byte reg = (byte) regNumber;
+        btMessage = String.format("W%02x%02x",reg,val);
+        return 0;
+    }
+
 }
